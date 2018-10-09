cc0c827962c2b0a91a873705178761ae02689826
CJ Faraon <faraonc@outlook.com>
2018-10-09 15:19:56 -0700

    WIP tutorial

--- main.go
+++ main.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"fmt"
-	"github.com/faraonc/hwsc-api-blocks/int/hwsc-grpc-sample/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+
+	pb"github.com/faraonc/hwsc-api-blocks/int/hwsc-grpc-sample/proto"
+	svr"github.com/faraonc/hwsc-grpc-sample/server"
 )
 
 func main() {
-	fmt.Println("Hello, playground")
-	grpc_sample_pb.RegisterSampleServiceServer()
+	fmt.Println("hwsc-grpc-sample Service is running...")
+
+	// Step 1: Make TCP listener
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil{
+		log.Fatalf("Failed to TCP listener %v", err)
+	}
+
+	// Step 2: Make gRPC server
+	s := grpc.NewServer()
+
+	// Step 3: Implement a server in a folder /server/server.go
+	// Step 4: Register gRPC server
+	pb.RegisterSampleServiceServer(s, svr.Server{})
+
+	// Step 5: Start gRPC server
+	if err := s.Serve(lis); err != nil{
+		log.Fatalf("Failed to serve %v", err)
+	}
 }
