3a53dd160912c19b88b2da6ac49abd2cb05c3d7e
CJ Faraon <faraonc@outlook.com>
2018-10-11 01:21:48 -0700

    Testing for camel case in proto

--- service/service.go
+++ service/service.go
@@ -10,5 +10,5 @@ type Service struct{}
 
 // This matches the proto in https://github.com/faraonc/hwsc-api-blocks/blob/master/int/hwsc-grpc-sample-svc/proto/grpc-sample-svc.proto
 func (s Service) SayHello(ctx context.Context, in *pb.SampleServiceRequest) (*pb.SampleServiceResponse, error) {
-	return &pb.SampleServiceResponse{Message: "Hello " + in.Name}, nil
+	return &pb.SampleServiceResponse{Message: "Hello " + in.FirstName}, nil
 }
