85f61444f92696ad6516202561f9fb4f9c28ffa3
CJ Faraon <faraonc@outlook.com>
2018-10-17 21:55:45 -0700

    performed linting

--- service/service.go
+++ service/service.go
@@ -8,9 +8,10 @@ import (
 	pb "github.com/faraonc/hwsc-api-blocks/int/hwsc-grpc-sample-svc/proto"
 )
 
+// Service implements the service SayHello defined in the proto.
 type Service struct{}
 
-// This matches the proto in https://github.com/faraonc/hwsc-api-blocks/blob/master/int/hwsc-grpc-sample-svc/proto/hwsc-grpc-sample-svc.proto
+// SayHello matches the proto in https://github.com/faraonc/hwsc-api-blocks/blob/master/int/hwsc-grpc-sample-svc/proto/hwsc-grpc-sample-svc.proto
 func (s Service) SayHello(ctx context.Context, in *pb.SampleServiceRequest) (*pb.SampleServiceResponse, error) {
 	fmt.Printf("Request { %v}\n", in)
 	fmt.Printf("Receiving UTC date: %s\n", time.Unix(in.GetCreateTimestamp(), 0).UTC())
